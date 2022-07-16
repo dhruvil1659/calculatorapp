8352abbc4b746c1966acb1041fd1821ebf50728e
dhruvil1659 <dhruvil1659@gmail.com>
2022-07-16 11:22:42 -0400

    Test cases created

--- src/test/java/com/miit/calculatorapp/calculatorapp/AppTest.java
+++ src/test/java/com/miit/calculatorapp/calculatorapp/AppTest.java
@@ -1,6 +1,7 @@
 package com.miit.calculatorapp.calculatorapp;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -13,8 +14,43 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test(){
+    	Calculator obj1 = new Calculator();
+    	int outputAdd = obj1.addition(2,3);
+    	assertEquals(5,outputAdd);
+    	
+  
+
     }
+    
+    @Test
+    public void test2(){
+    	
+    	
+    	Calculator obj2 = new Calculator();
+    	int outputSub = obj2.subtraction(3,1);
+    	assertEquals(2,outputSub);
+    	
+    	
+    }
+    
+    @Test
+    public void test3(){
+    	
+    	Calculator obj3 = new Calculator();
+    	int outputMulti = obj3.multiplication(3,2);
+    	assertEquals(6,outputMulti);
+
+    }
+    
+//    @Test
+//    public void test4(){
+//    	
+//    	Calculator obj4 = new Calculator();
+//    	float outputDivi = obj4.division(3,1);
+//    	assertEquals(3,outputDivi);
+//
+//    }
+//    
+	   
 }
